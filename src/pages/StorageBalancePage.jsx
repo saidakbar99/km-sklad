15fc47011c4393e00c15e2e8bae590f51f6da47a
Muhammadrahim <muqalla21@gmail.com>
2025-02-08 19:06:16 +0500

    added furniture select, search user, keeping

--- src/pages/StorageBalancePage.jsx
+++ src/pages/StorageBalancePage.jsx
@@ -1,110 +1,157 @@
-import { useState } from "react";
+import {useState} from "react";
 import MainLayout from "../components/MainLayout";
-import { DataTable } from "primereact/datatable";
-import { Column } from "primereact/column";
-import { InputText } from "primereact/inputtext";
-import { Dropdown } from "primereact/dropdown";
+import {DataTable} from "primereact/datatable";
+import {Column} from "primereact/column";
+import {InputText} from "primereact/inputtext";
+import {Dropdown} from "primereact/dropdown";
 import ExportButton from "../components/ExcelButton";
+import {Bookmark, BookmarkCheck} from "lucide-react";
 
 const placementOptions = [
-  { label: "Hamma bloklar", value: "all" },
-  { label: "A1-blok", value: "a1" },
-  { label: "A2-blok", value: "a2" },
-  { label: "B1-blok", value: "b1" },
-  { label: "B2-blok", value: "b2" },
-  { label: "C1-blok", value: "c1" },
-  { label: "C2-blok", value: "c2" },
+	{label: "Hamma bloklar", value: "all"},
+	{label: "A1-blok", value: "a1"},
+	{label: "A2-blok", value: "a2"},
+	{label: "B1-blok", value: "b1"},
+	{label: "B2-blok", value: "b2"},
+	{label: "C1-blok", value: "c1"},
+	{label: "C2-blok", value: "c2"},
 ];
 
 const bundleOptions = [
-  { label: "Hamma komplektlar", value: "all" },
-  { label: "Босфор", value: "123" },
-  { label: "Шедевр", value: "222" },
-  { label: "Граф", value: "4112" },
-  { label: "Барокко", value: "A234" },
-  { label: "Принц", value: "B231" },
-  { label: "Дукале", value: "E213" },
+	{label: "Hamma komplektlar", value: "all"},
+	{label: "Босфор", value: "123"},
+	{label: "Шедевр", value: "222"},
+	{label: "Граф", value: "4112"},
+	{label: "Барокко", value: "A234"},
+	{label: "Принц", value: "B231"},
+	{label: "Дукале", value: "E213"},
 ];
 
 const categoryOptions = [
-  { label: "Hamma kategoriyalar", value: "all" },
-  { label: "СП", value: "A12" },
-  { label: "Стул", value: "B21" },
-  { label: "Стол", value: "D22" },
-  { label: "Мягкий", value: "A2664" },
+	{label: "Hamma kategoriyalar", value: "all"},
+	{label: "СП", value: "A12"},
+	{label: "Стул", value: "B21"},
+	{label: "Стол", value: "D22"},
+	{label: "Мягкий", value: "A2664"},
+];
+
+const furnitureOptions = [
+	{label: "Hamma mebellar", value: "all"},
+	{label: "Kravat", value: "Krovat"},
+	{label: "stul", value: "Stul"},
 ];
 
 const StorageBalancePage = () => {
-  const [filters, setFilters] = useState({
-    placement: 'all',
-    bundle: 'all',
-    category: 'all',
-  });
-  const [searchText, setSearchText] = useState("");
+	const [filters, setFilters] = useState({
+		placement: "all",
+		users: "",
+		clients: "all",
+		bundle: "all",
+		category: "all",
+		furniture: "all",
+	});
+	const [searchText, setSearchText] = useState("");
+	const [selectedRow, setSelectedRow] = useState([]);
 
-  const data = Array.from({ length: 5 }, (_, i) => ({
-    id: i + 1,
-    clientId: '010AB',
-    clientName: 'Farxod aka',
-    bundle: 'SP / Barokko',
-    furniture: i % 2 === 0 ? 'Krovat' : 'Stul',
-    quantity: i % 2 === 0 ? 1 : 2,
-    // date: new Date().toLocaleDateString(),
-  }));
+	const data = Array.from({length: 5}, (_, i) => ({
+		id: i + 1,
+		clientId: "010AB",
+		clientName: "Farxod aka",
+		bundle: "SP / Barokko",
+		furniture: i % 2 === 0 ? "Krovat" : "Stul",
+		quantity: i % 2 === 0 ? 1 : 2,
+		selected: !selectedRow.includes(++i) ? (
+			<Bookmark
+				className="cursor-pointer"
+				onClick={() => setSelectedRow([...selectedRow, i])}
+			/>
+		) : (
+			<BookmarkCheck
+				className="text-red-400 cursor-pointer"
+				onClick={() => {
+					const filteredIds = selectedRow.filter((value) => value !== i);
+					setSelectedRow([...filteredIds]);
+				}}
+			/>
+		),
+		// date: new Date().toLocaleDateString(),
+	}));
 
-  const filteredData = data.filter((item) =>
-    item.clientId.toLowerCase().includes(searchText.toLowerCase())
-  );
+	let filteredData = data.filter((item) =>
+		item.clientId.toLowerCase().includes(searchText.toLowerCase())
+	);
 
-  return (
-    <MainLayout header='Sklad ostatok'>
-      <div className="p-6">
-        <div className="flex mb-4">
-          <Dropdown 
-            value={filters.placement} 
-            options={placementOptions} 
-            onChange={(e) => setFilters({...filters, placement: e.value})} 
-            className="max-w-[300px] border mr-4"
-            placeholder="asdasd"
-          />
-          <Dropdown 
-            value={filters.bundle} 
-            options={bundleOptions} 
-            onChange={(e) => setFilters({...filters, bundle: e.value})} 
-            className="max-w-[300px] border mr-4"
-          />
-          <Dropdown 
-            value={filters.category} 
-            options={categoryOptions} 
-            onChange={(e) => setFilters({...filters, category: e.value})} 
-            className="max-w-[300px] border"
-          />
-          <InputText
-            value={searchText}
-            onChange={(e) => setSearchText(e.target.value)}
-            placeholder="Qidirish..."
-            className="px-4 pt-1 border mx-4 w-full flex"
-          />
-          <ExportButton data={filteredData} fileName='Склад Остаток' />
-        </div>
+	if (filters.furniture !== "all" || filters.users) {
+		filteredData = data.filter((item) => item.furniture === filters.furniture);
+	}
 
-        <DataTable 
-          value={filteredData} 
-          paginator 
-          rows={10} 
-          rowsPerPageOptions={[10, 25, 50, 100]}
-        >
-          <Column field="id" header="N" />
-          <Column field="clientId" header="Заказ" />
-          <Column field="clientName" header="Клиент" />
-          <Column field="bundle" header="Комплект" />
-          <Column field="furniture" header="Мебель" />
-          <Column field="quantity" header="Сони" />
-          {/* <Column field="date" header="Сана" /> */}
-        </DataTable>
-      </div>
-    </MainLayout>
-  );
+	return (
+		<MainLayout header="Sklad ostatok">
+			<div className="p-6">
+				<div className="flex mb-4 max-[1350px]:grid max-[1350px]:grid-cols-4 gap-4 max-[900px]:grid-cols-2 max-[480px]:grid-cols-1">
+					<Dropdown
+						value={filters.category}
+						options={categoryOptions}
+						onChange={(e) => setFilters({...filters, category: e.value})}
+						className="border "
+					/>
+					<Dropdown
+						value={filters.bundle}
+						options={bundleOptions}
+						onChange={(e) => setFilters({...filters, bundle: e.value})}
+						className="border"
+					/>
+					<Dropdown
+						value={filters.placement}
+						options={placementOptions}
+						onChange={(e) => setFilters({...filters, placement: e.value})}
+						className="border"
+						placeholder="asdasd"
+					/>
+					<Dropdown
+						value={filters.furniture}
+						options={furnitureOptions}
+						onChange={(e) => setFilters({...filters, furniture: e.value})}
+						className="border "
+					/>
+					<InputText
+						className="pl-3 border max-[480px]:h-[2.5em] h-[2.5em]"
+						placeholder="Mijozlarni qidiring..."
+						onChange={(e) => setFilters({...filters, users: e.target.value})}
+					/>
+					<InputText
+						value={searchText}
+						onChange={(e) => setSearchText(e.target.value)}
+						placeholder="Qidirish..."
+						className="flex w-full px-4 pt-1 border max-[1350px]:col-span-2 max-[900px]:col-span-1 max-[480px]:h-[2.5em]"
+					/>
+					<ExportButton
+						className="col-span-2"
+						data={filteredData}
+						fileName="Склад Остаток"
+					/>
+				</div>
+
+				<DataTable
+					value={filteredData}
+					paginator
+					rows={10}
+					rowsPerPageOptions={[10, 25, 50, 100]}
+					rowClassName={(value) =>
+						selectedRow.includes(value.id) ? "selected-row" : ""
+					}>
+					<Column field="id" header="N" />
+					<Column field="clientId" header="Заказ" />
+					<Column field="clientName" header="Клиент" />
+					<Column field="bundle" header="Комплект" />
+					<Column field="furniture" header="Мебель" />
+					<Column field="quantity" header="Сони" />
+					<Column field="selected" header="tanlangan" />
+					{/* <Column field="date" header="Сана" /> */}
+				</DataTable>
+			</div>
+		</MainLayout>
+	);
 };
 
 export default StorageBalancePage;
