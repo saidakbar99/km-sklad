a1750eeab3da723f933dc61fa689f012e0351b55
Muhammadrahimw <muqalla21@gmail.com>
2025-02-09 12:17:31 +0500

    added qr code

--- src/pages/GeneratedSerialsPage.jsx
+++ src/pages/GeneratedSerialsPage.jsx
@@ -1,58 +1,58 @@
-import { useState, useEffect } from "react";
-import { GeneratedSerial } from "../components/GeneratedSerial";
+import {useState, useEffect} from "react";
+import {GeneratedSerial} from "../components/GeneratedSerial";
 // import { connectQZTray, getPrinters } from "../utils/qzHelper";
 import MainLayout from "../components/MainLayout";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 import axios from "axios";
 
 const GeneratedSerialsPage = () => {
-  // const [printers, setPrinters] = useState([]);
-  // const [selectedPrinter, setSelectedPrinter] = useState("");
-  const [serials, setSerials] = useState([])
-  // const [loading, setLoading] = useState(true);
-  // const [error, setError] = useState(null);
+	// const [printers, setPrinters] = useState([]);
+	// const [selectedPrinter, setSelectedPrinter] = useState("");
+	const [serials, setSerials] = useState([]);
+	// const [loading, setLoading] = useState(true);
+	// const [error, setError] = useState(null);
 
-  useEffect(() => {
-      // const initializeQZTray = async () => {
-      //   try {
-      //     await connectQZTray();
-      //     const printers = await getPrinters();
-      //     setPrinters(printers);
-      //   } catch (err) {
-      //     setError("Failed to connect to QZ Tray or retrieve printers.");
-      //   } finally {
-      //     setLoading(false);
-      //   }
-      // };
-  
-      // initializeQZTray();
-      
-      const fetchGeneratedSerials = async () => {
-        const response = await axios.get("http://localhost:5000/api/serials")
-        setSerials(response.data.serials)
-      }
+	useEffect(() => {
+		// const initializeQZTray = async () => {
+		//   try {
+		//     await connectQZTray();
+		//     const printers = await getPrinters();
+		//     setPrinters(printers);
+		//   } catch (err) {
+		//     setError("Failed to connect to QZ Tray or retrieve printers.");
+		//   } finally {
+		//     setLoading(false);
+		//   }
+		// };
 
-      fetchGeneratedSerials()
-    }, []);
-  
-    // if (loading) {
-    //   return <div>Loading printers...</div>;
-    // }
-  
-    // if (error) {
-    //   return <div>{error}</div>;
-    // }
+		// initializeQZTray();
 
-  return (
-    <MainLayout header='Yaratilgan seriya nomerlar'>
-      <div className="max-w-[1140px] mx-auto">
-        <div className="mb-6 flex justify-between items-center">
-          <div>
-            <h2 className="text-lg font-bold">Printerni tanlash</h2>
-            {/* <select
+		const fetchGeneratedSerials = async () => {
+			const response = await axios.get("http://localhost:5000/api/serials");
+			setSerials(response.data.serials);
+		};
+
+		fetchGeneratedSerials();
+	}, []);
+
+	// if (loading) {
+	//   return <div>Loading printers...</div>;
+	// }
+
+	// if (error) {
+	//   return <div>{error}</div>;
+	// }
+
+	return (
+		<MainLayout header="Yaratilgan seriya nomerlar">
+			<div className="max-w-[1140px] mx-auto">
+				<div className="flex items-center justify-between mb-6">
+					<div>
+						<h2 className="text-lg font-bold">Printerni tanlash</h2>
+						{/* <select
               value={selectedPrinter}
               onChange={(e) => setSelectedPrinter(e.target.value)}
-              className="border p-3 mt-2 rounded-lg"
+              className="p-3 mt-2 border rounded-lg"
             >
               <option value="">Printerni tanlang</option>
               {printers.map((printer, index) => (
@@ -61,26 +61,29 @@ const GeneratedSerialsPage = () => {
                 </option>
               ))}
             </select> */}
-          </div>
-          <Link to='/generation'>
-            <button className="w-full px-4 py-2 text-white bg-blue rounded-md hover:bg-opacity-90">
-              Generatsiya qilish
-            </button>
-          </Link>
-        </div>
-        {serials.length ? (
-          serials.map(serial => {
-            return (
-              <GeneratedSerial selectedPrinter={'selectedPrinter'} />
-            )
-          })
-        ) : (
-          <div>Seriaya nomerlarni yaratilmagan</div>
-        )}
-        
-      </div>
-    </MainLayout>
-  );
-}
+					</div>
+					<Link to="/generation">
+						<button className="w-full px-4 py-2 text-white rounded-md bg-blue hover:bg-opacity-90">
+							Generatsiya qilish
+						</button>
+					</Link>
+				</div>
+				{serials.length ? (
+					serials.map((serial) => {
+						return (
+							<GeneratedSerial
+								key={serial.id}
+								serial={serial}
+								selectedPrinter={"selectedPrinter"}
+							/>
+						);
+					})
+				) : (
+					<div>Seriaya nomerlarni yaratilmagan</div>
+				)}
+			</div>
+		</MainLayout>
+	);
+};
 
 export default GeneratedSerialsPage;
