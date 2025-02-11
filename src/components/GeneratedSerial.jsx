e515003c4867764cebcee35932bffe9b817d6aaa
Saidakbar <saidakbar.dev@gmail.com>
2025-02-11 21:17:12 +0500

    added komplekt name to serials

--- src/components/GeneratedSerial.jsx
+++ src/components/GeneratedSerial.jsx
@@ -11,20 +11,25 @@ export const GeneratedSerial = ({selectedPrinter, serial}) => {
 		printLabel(selectedPrinter);
 	};
 
-	console.log(serial);
+	const category = serial.furniture.category_furniture.name
+	const komplekt = serial.furniture.komplekt_furniture[0].komplekt.name
+	const mebel = serial.furniture.name
+	const demandNumber = serial?.demand_furniture?.demand?.doc_no
+	const color = serial?.unique?.color?.name || serial?.demand_furniture?.color?.name
 
 	return (
 		<div className="flex items-center mb-12">
 			<div className="flex items-center w-full gap-8 p-8 border-2 rounded-2xl">
 				<QRCodeCanvas value={serial.unique.name} size={200} />
-				<div className="">
+				<div className="w-full">
 					<p className="mb-4 text-4xl">Серия# {serial.unique.name}</p>
 					{serial.demand_furniture && (
-						<p className="mb-4 text-4xl">Заказ# {serial?.demand_furniture?.demand?.doc_no}</p>
+						<p className="mb-4 text-4xl">Заказ# {demandNumber}</p>
 					)}
-					<p className="text-[28px] mb-4">{serial.furniture.category_furniture.name} {serial.furniture.name}</p>
-					<p className="text-lg text-right">Ранги: {serial?.unique?.color?.name || serial?.demand_furniture?.color?.name}</p>
-					<p></p>
+					<p className="text-[28px] mb-4">{category} {komplekt} {mebel}</p>
+					{color && (
+						<p className="text-lg text-right">Ранги: {color}</p>
+					)}
 				</div>
 			</div>
 			<div className="mx-8 cursor-pointer" onClick={handlePrint}>
